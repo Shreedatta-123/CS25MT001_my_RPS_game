0c78c7b35333323bc993d4fa9240a1654c1f6f5b
Shreedatta Nadig <cs25mt001@iitdh.ac.in>
2025-08-24 23:13:54 +0530

    Added game rules

--- RPS.py
+++ RPS.py
@@ -1,3 +1,9 @@
+import random
+
 if __name__ == "__main__":
-    print("Welcome to Rock, Paper, Scissors game!")
-    
+    print("Welcome to Rock, Paper, Scissors game!\n")
+    print("""Rules of the game: Two players secretly pick one of "rock," "paper," or "scissors". 
+Both players reveal their selection to the other player at once; the winner is chosen
+based on what the selections are. Rock beats scissors (by crushing them); scissors
+beats paper (by cutting it); and paper beats rock (by covering it). If both players select
+the same one, it is a tie""")
