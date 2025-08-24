fe0a43c44e1f6492012d9cf9b88f81495a51c98d
Shreedatta Nadig <cs25mt001@iitdh.ac.in>
2025-08-24 23:23:33 +0530

    Final fully functional game code

--- RPS.py
+++ RPS.py
@@ -15,4 +15,14 @@ the same one, it is a tie""")
             user_choice = input("Invalid choice. Please enter rock, paper, or scissors (lowercase): ")
             continue
             print(f"Computer chose: {computer_choice}")
-        
+        if user_choice == computer_choice:
+            print("It's a tie!")
+            user_choice = input("Enter your choice (rock, paper, scissors): ")
+            continue
+        elif (user_choice == "rock" and computer_choice == "scissors") or \
+            (user_choice == "paper" and computer_choice == "rock") or \
+            (user_choice == "scissors" and computer_choice == "paper"):
+            print("You win!")
+        else:
+            print("Computer wins!")
+        break
