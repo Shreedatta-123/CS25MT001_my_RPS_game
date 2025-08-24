4084d70ae05aa5c0398a9f7abaca0dd5678fd9a8
Shreedatta Nadig <cs25mt001@iitdh.ac.in>
2025-08-25 00:03:23 +0530

    Final bugFix for computer choice

--- RPS.py
+++ RPS.py
@@ -6,18 +6,19 @@ if __name__ == "__main__":
 Both players reveal their selection to the other player at once; the winner is chosen
 based on what the selections are. Rock beats scissors (by crushing them); scissors
 beats paper (by cutting it); and paper beats rock (by covering it). If both players select
-the same one, it is a tie""")
+the same one, it is a tie\n""")
     selection = ["rock", "paper", "scissors"]
     computer_choice = random.choice(selection)
-    user_choice = input("Enter your choice (rock, paper, scissors): ")
+    user_choice = input("Enter your choice in lowercase (rock, paper, scissors): ")
     while True:
         if user_choice not in selection:
             user_choice = input("Invalid choice. Please enter rock, paper, or scissors (lowercase): ")
             continue
-            print(f"Computer chose: {computer_choice}")
+        print(f"Computer chose: {computer_choice}")
         if user_choice == computer_choice:
             print("It's a tie!")
-            user_choice = input("Enter your choice (rock, paper, scissors): ")
+            computer_choice = random.choice(selection)
+            user_choice = input("Enter your choice in lowercase (rock, paper, scissors): ")
             continue
         elif (user_choice == "rock" and computer_choice == "scissors") or \
             (user_choice == "paper" and computer_choice == "rock") or \
